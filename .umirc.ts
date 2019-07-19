fb144cb76160ca385de84fba3a3abfbcc04c6fbb
MrHeer <hlm52pk@163.com>
2019-07-19 22:13:00 +0800

    feat: add gh-pages and react-mathjax

--- .umirc.ts
+++ .umirc.ts
@@ -2,14 +2,18 @@ import { IConfig } from 'umi-types';
 
 // ref: https://umijs.org/config/
 const config: IConfig =  {
+  base: '/MathJaxPlayground/',
+  publicPath: '/MathJaxPlayground/',
   treeShaking: true,
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
+    'umi-plugin-gh-pages',
     ['umi-plugin-react', {
       antd: true,
       dva: false,
       dynamicImport: { webpackChunkName: true },
-      title: 'create',
+      exportStatic: true,
+      title: 'MathJaxPlayground',
       dll: true,
       locale: {
         enable: true,
