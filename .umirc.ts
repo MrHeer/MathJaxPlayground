9d830c1d4cdde231d057bf80467a3352e14f8e9b
MrHeer <hlm52pk@163.com>
2021-12-06 09:32:42 +0800

    chore: deploy

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  base: '/MathJaxPlayground/',
+  publicPath: '/MathJaxPlayground/',
   nodeModulesTransform: {
     type: 'none',
   },
